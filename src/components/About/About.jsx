7f0fc36348d5531d32fd3a50ff9df54b7430a58e
Galartza <jotaquin99g@gmail.com>
2024-04-08 18:41:40 -0300

    Agregando componentes0.2

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import img from '../../assets/img-about.png'
 import '../About/about.css'
 
 export const About = () => {
   return (
     <div className='about-container'>
-      <h1>About</h1>
+      <h1>About me</h1>
 
-      <p className='text-aboutme'>
-        Mi nombre es Joaquín Galarza, soy estudiante de tercer año en la Tecnicatura en Desarrollo de Software.
-        Más allá de mis estudios, he canalizado mi pasión por la tecnología dedicándome al aprendizaje autodidacta del desarrollo web Front-end mediante cursos y tutoriales en línea.
-        Además, soy un apasionado jugador de basquetbol adaptado.
-      </p>
+      <div className='container-about'>
+        <div className="conteiner-text">
+          <p className='text-aboutme'>
+            Mi nombre es Joaquín Galarza, soy estudiante de tercer año en la Tecnicatura en Desarrollo de Software.
+            Más allá de mis estudios, he canalizado mi pasión por la tecnología dedicándome al aprendizaje autodidacta del desarrollo web Front-end mediante cursos y tutoriales en línea.
+            Además, soy un apasionado jugador de basquetbol adaptado.
+          </p>
+        </div>
+
+        <img className='img-about' src={img} alt="" />
+      </div>
     </div>
   )
 }
