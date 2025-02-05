bd61dd8faa443290f99ef49a281be2ddd2438d67
Galartza <jotaquin99g@gmail.com>
2025-02-05 17:36:58 -0300

    ok, no more

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@
 import './index.css';
 import Navigation from './components/Navigation/Navigation';
 import { Routes, Route, Navigate } from 'react-router-dom'; // No es necesario BrowserRouter aquí
-import { About } from './pages/About/About';
-import { Portfolio } from './pages/Portfolio/Portfolio';
-import { Contact } from './pages/Contact/Contact';
-import { Home } from './pages/Home/Home';
+import { About } from './pages/about/About';
+import { Portfolio } from './pages/portfolio/Portfolio';
+import { Contact } from './pages/contact/Contact';
+import { Home } from './pages/home/Home';
 
 function App() {
   return (
     <>
       <Navigation />
       <Routes>
-        <Route path="/" element={<Navigate to="/" />} /> {/* Redirige a /home */}
+        <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/portfolio" element={<Portfolio />} />
